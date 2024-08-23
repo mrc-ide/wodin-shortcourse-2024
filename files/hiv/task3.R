6f651efe9dfdd4a5c975224a833f4e2ca28d05aa
Lilith Whittles <lwhittles@users.noreply.github.com>
2024-08-23 12:01:08 +0100

    Romain changes 2024

--- files/hiv/task3.R
+++ files/hiv/task3.R
@@ -1,14 +1,14 @@
-##Differential equations
+## Differential equations
 
 deriv(S) <- B-beta*S*I/N-mu*S
 deriv(I) <- beta*S*I/N-mu*I-sigma*I
 
-##Initial conditions
+## Initial conditions
 
 initial(S) <- 995
 initial(I) <- 5
 
-##Other equations
+## Other equations
 
 N <- S+I
 
@@ -20,7 +20,7 @@ B <- alpha*N 	#entry rate, exponentially growing population
 R0 <- beta/(mu+sigma)
 
 
-##Parameter values
+## Parameter values
 
 c <-user(2.4)		    #partner change rate
 kappa <- user(0.1)  	#per partner HIV transmission probability
@@ -28,9 +28,9 @@ sigma <- user(0.0833)      #mortality rate per person per year due to HIV/AIDS (
 mu <- user(0.008)      #crude mortality rate due to causes other than AIDS, scaled to rate per person
 alpha <-user(0.0332)    #birth rate scaled to per person
 
-##Additional output
+## Additional outputs
 
 output(prevalence) <- I/N
-output(Preveq) <- 1-1/R0 #prevalence at equilibrium
+output(Preveq) <- 1-1/R0   # prevalence at equilibrium
 output(R0_out) <-beta/(mu+sigma)
 output(incidence) <- (beta*S*I/N)/S
